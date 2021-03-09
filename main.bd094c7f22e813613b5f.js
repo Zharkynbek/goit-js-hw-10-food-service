23d21e4936d90ad17603aa840b46363f4f57f63b
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-03-09 12:51:58 +0100

    Updates

---
+++ main.bd094c7f22e813613b5f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{G2cz:function(n,i){alert("privet")},L1EO:function(n,i,c){},QfWi:function(n,i,c){"use strict";c.r(i);c("L1EO"),c("G2cz")}},[["QfWi",1]]]);
+//# sourceMappingURL=main.bd094c7f22e813613b5f.js.map
